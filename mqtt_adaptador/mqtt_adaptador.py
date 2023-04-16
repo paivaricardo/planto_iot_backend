176abd94bca7f78bb9d57e6aa9527914d8739913
Ricardo Paiva <ricardoclpaiva@gmail.com>
2023-04-16 17:26:45 -0300

    Implementação parcial do adaptador MQTT em loop.

--- mqtt_adaptador/mqtt_adaptador.py
+++ mqtt_adaptador/mqtt_adaptador.py
@@ -10,21 +10,29 @@ class MQTTAdapter:
 
             # MQTT client instance
             cls._instance.client = mqtt.Client()
+
+            # MQTT client callbacks
             cls._instance.client.on_connect = cls._instance.on_connect
             cls._instance.client.on_message = cls._instance.on_message
+
             # Connect to the MQTT broker
             cls._instance.client.connect("18.214.223.254", 1883, 60)
-            cls._instance.client.loop_start()
+
+            # Subscribe to the desired topics
+            cls._instance.client.subscribe("planto-iot-sensores")
+
+            cls._instance.client.loop_forever()
         return cls._instance
 
     def on_connect(self, client, userdata, flags, result_code):
         print("Conectado à instância MQTT com o código de resultado ", str(result_code))
-        # Subscribe to the desired topics
-        client.subscribe("planto-iot-sensores")
 
     def on_message(self, client, userdata, msg):
         print("Recebida mensagem via MQTT no tópico {" + msg.topic + "}: " + str(msg.payload))
+
         # Process the received message
+
+
 
     def publish(self, topic, payload):
         # Publish a message to the MQTT broker
