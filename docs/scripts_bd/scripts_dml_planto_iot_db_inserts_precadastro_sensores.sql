b4eba3bb461ca6736742e4be143ab493c7ce3907
Ricardo Paiva <ricardoclpaiva@gmail.com>
2023-05-14 21:22:36 -0300

    Pequenas alterações no Script DML para precadastro.

--- docs/scripts_bd/scripts_dml_planto_iot_db_inserts_precadastro_sensores.sql
+++ docs/scripts_bd/scripts_dml_planto_iot_db_inserts_precadastro_sensores.sql
@@ -1,7 +1,8 @@
 -- ************************************** SCRIPTS DML para precadastro manual de sensores
 -- ************************************** Database: db_planto_iot_sensores
 
-
+-- ************************************** Sensor virtualizado de umidade do solo - Ulisses
+-- ************************************** UUID: b97b7ac5-7167-4a31-b4b8-b67c987ddf5b
 INSERT INTO tb_sensor_atuador
     (uuid_sensor_atuador, id_tipo_sensor)
 VALUES('b97b7ac5-7167-4a31-b4b8-b67c987ddf5b', 10000);
