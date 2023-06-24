37ca11cdf58eae2efc099b3d695abbb71a061cb6
Ricardo <ricardoclpaiva@gmail.com>
2023-06-24 17:58:54 -0300

    Correção de bugs no serviço para envio de mensagens para reconectar sensores/atuadores com novas áreas, de acordo com o cadastro.

--- mdl_interacao_sensores_atuadores/interacao_geral_sensores_atuadores.py
+++ mdl_interacao_sensores_atuadores/interacao_geral_sensores_atuadores.py
@@ -2,21 +2,22 @@ import datetime
 import json
 import logging
 
+from model.query_models.sensor_atuador_query_model import SensorAtuadorQueryModel
 from mqtt_adaptador.mqtt_adaptador import MQTTAdapter
 
 
-def enviar_informacao_conexao_area_sensor_atuador(sensor_atuador_query_model: dict):
+def enviar_informacao_conexao_area_sensor_atuador(sensor_atuador_query_model: SensorAtuadorQueryModel):
     """
     Envia informação de área do sensor/atuador para o broker MQTT, que servirá para a conexão dos sensores e atuadores virtualizados a determinada área, indicada pelo id da área.
     """
     try:
-        topico_info_conexao_area = f"planto-iot-sensores/{sensor_atuador_query_model['uuid_sensor_atuador']}"
+        topico_info_conexao_area = f"planto-iot-sensores/{sensor_atuador_query_model.uuid_sensor_atuador}"
 
         payload_mensagem_conexao_area = {
-            "uuidSensorAtuador": sensor_atuador_query_model['uuid_sensor_atuador'],
+            "uuidSensorAtuador": sensor_atuador_query_model.uuid_sensor_atuador,
             "dataHoraConexaoArea": datetime.datetime.now(),
             "tipoSinal": 60100,
-            "idAreaConectada": sensor_atuador_query_model['area']['id_area'],
+            "idAreaConectada": sensor_atuador_query_model.area.id_area,
         }
 
         # Instanciar o adaptador MQTT (instância singleton do adaptador MQTT)
@@ -26,7 +27,7 @@ def enviar_informacao_conexao_area_sensor_atuador(sensor_atuador_query_model: di
         mqtt_adapter.publish(topico_info_conexao_area, json.dumps(payload_mensagem_conexao_area))
 
         logging.info(
-            f"[INTER. ATUADORES - INFO] Enviado sinal de conexão com a área {sensor_atuador_query_model['area']['id_area']} para o sensor/atuador de UUID {sensor_atuador_query_model['uuid_sensor_atuador']} com sucesso!")
+            f"[INTER. ATUADORES - INFO] Enviado sinal de conexão com a área {sensor_atuador_query_model.area.id_area} para o sensor/atuador de UUID {sensor_atuador_query_model.uuid_sensor_atuador} com sucesso!")
 
         return True
     except Exception as e:
