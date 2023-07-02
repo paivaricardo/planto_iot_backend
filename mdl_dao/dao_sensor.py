0f48c7b957df72491c452a470444acd44e84c784
Ricardo <ricardoclpaiva@gmail.com>
2023-07-02 18:49:11 -0300

    Mudança do modelo de sensor em SensorAtuador para SensorAtuadorQueryModel, contendo mais informações.

--- mdl_dao/dao_sensor.py
+++ mdl_dao/dao_sensor.py
@@ -3,14 +3,14 @@ import logging
 from sqlalchemy.exc import SQLAlchemyError
 
 from mdl_dao import database
-from model.sensor_atuador_model import SensorAtuador
+from model.query_models.sensor_atuador_query_model import SensorAtuadorQueryModel
 
 
 def obter_sensores_bd():
     session = database.create_session()
 
     try:
-        sensores = session.query(SensorAtuador).all()
+        sensores = session.query(SensorAtuadorQueryModel).all()
         return sensores
 
     except SQLAlchemyError as e:
